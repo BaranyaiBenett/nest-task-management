27f87bcd31c4c9019d74c8a20eb88e7c93e81b29
BaranyaiBenett <baranyai.benett@gmail.com>
2022-03-17 14:17:51 +0100

    init

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.dev`], // STAGE will dictate which env file will be loaded, package.json includes the STAGE variable
+      validationSchema: configValidationSchema,
+    }),
+    TasksModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    AuthModule,
+  ],
+})
+export class AppModule {}
---
+++ src/tasks/tasks.module.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { TasksRepository } from './dto/tasks.repository';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+
+@Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([TasksRepository]),
+    AuthModule],
+  controllers: [TasksController],
+  providers: [TasksService],
+})
+export class TasksModule {}
